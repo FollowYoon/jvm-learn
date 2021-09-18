d1ef8280d79c5983598b50b42b9cefa8d25a2e4e
秦东 <qind@izkml.com>
2021-09-18 17:30:56 +0800

    first commit

---
+++ src/main/java/com/jvm/taskw102/OwnClassLoader.java
@@ -0,0 +1,46 @@
+package com.jvm.taskw102;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author zyj
+ * @date 2021/9/18 16:48
+ */
+public class OwnClassLoader extends ClassLoader {
+
+    public static void main(String[] args) throws Exception {
+        String className = "Hello";
+        String methodName = "hello";
+
+        ClassLoader classLoader = new OwnClassLoader();
+        Class<?> clazz = classLoader.loadClass(className);
+        Object classInst = clazz.newInstance();
+        clazz.getMethod(methodName).invoke(classInst);
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String suffix = ".xlass";
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(name + suffix);
+        try{
+            int length = inputStream.available();
+            byte[] bytes = new byte[length];
+            inputStream.read(bytes);
+
+            byte[] decodeBytes = new byte[length];
+            for (int i = 0; i < length; i++) {
+                decodeBytes[i] = (byte) (255 - bytes[i]);
+            }
+            return defineClass(name, decodeBytes,0, length);
+        }catch (IOException e){
+            throw new ClassNotFoundException(name, e);
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
