a991608449b2b38ca52352f4c99db1ab35c203e4
秦东 <qind@izkml.com>
2021-12-07 17:01:25 +0800

    第12周作业1：配置 redis 的主从复制，sentinel 高可用，Cluster 集群

---
+++ 12/12-1/bash/集群启动脚本.sh
@@ -0,0 +1,7 @@
+cd /data/redis/cluster/bin
+./redis-server ../cluster/7001/redis.conf
+./redis-server ../cluster/7002/redis.conf
+./redis-server ../cluster/7003/redis.conf
+./redis-server ../cluster/7004/redis.conf
+./redis-server ../cluster/7005/redis.conf
+./redis-server ../cluster/7006/redis.conf
